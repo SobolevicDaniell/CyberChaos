d22aceccf8e96a4d74b2ab723da96fb3f1b88bb8
SobolevicDaniell <sobolevdan99@yandex.ru>
2023-02-03 14:36:38 +0300

    Add money

--- Assets/Scripts/MONEY.cs
+++ Assets/Scripts/MONEY.cs
@@ -1,20 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class GameManager : MonoBehaviour {
+public class MONEY : MonoBehaviour {
+
+
     public int money;
     public TextMeshProUGUI moneyText;
+    [SerializeField] private int amount = 1;
 
-    public static GameManager instance;
+    public static MONEY instance;
+
+    private void Start()
+    {
+        moneyText.text = "$" + money.ToString();
+        
+    }
 
     void Awake() {
         instance = this;
     }
 
-    public void AddMoney(int amount) {
+    public void AddMoney() {
         money += amount;
         moneyText.text = "$" + money.ToString();
     }
